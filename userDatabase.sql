bb1081a1000348a8689261fadffad5fab2c98b7d
gmobi-wush <wush.wu@generalmobi.com>
2016-12-22 17:55:44 +0800

    Update userDatabase.sql

--- userDatabase.sql
+++ userDatabase.sql
@@ -1,9 +1,24 @@
-SELECT
-	identity,
-	appTable.apps AS apps,
-	iabTable.iabs AS iabs
-FROM
-	`userdata.groupedIdAppUserData` AS appTable
-FULL JOIN
-	`userdata.groupedIdIabUserData` AS iabTable
-ON appTable.identity = iabTable.identity
+WITH 
+joinTable AS (
+	SELECT
+		appTable.identity AS identity1,
+		iabTable.identity AS identity2,
+		appTable.apps AS apps,
+		iabTable.iabs AS iabs
+	FROM
+		`userdata.groupedIdAppUserData` AS appTable
+	FULL JOIN
+		`userdata.groupedIdIabUserData` AS iabTable
+	ON appTable.identity = iabTable.identity
+),
+tb1 AS (
+	SELECT identity1 AS identity, apps, iabs
+	FROM joinTable
+	WHERE identity1 IS NOT NULL
+),
+tb2 AS (
+	SELECT identity2 AS identity, apps, iabs 
+	FROM joinTable
+	WHERE identity1 IS NULL
+)
+SELECT * FROM tb1 UNION ALL SELECT * FROM tb2
