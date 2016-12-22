d8fa55bb9292555d1574d9f7176ca884859cb0c3
gmobi-wush <wush.wu@generalmobi.com>
2016-12-22 17:08:15 +0800

    Update DailyUserDatabase.sh

--- DailyUserDatabase.sh
+++ DailyUserDatabase.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 
 (bq rm -f userdata.groupedIdAppUserData || echo "userdata.groupedIdAppUserData does not exist") && \
-cat groupedIdAppUserData.sql | bq query --nouse_legacy_sql --allow_large_results --destination_table 'userdata.groupedIdAppUserData'
+(cat groupedIdAppUserData.sql | bq query --nouse_legacy_sql --allow_large_results --destination_table 'userdata.groupedIdAppUserData') && \
+(bq rm -f userdata.userDatabase || echo "userdata.userDatabase does not exist") && \
+(cat userDatabase.sql | bq query --nouse_legacy_sql --allow_large_results --destination_table 'userdata.userDatabase')
