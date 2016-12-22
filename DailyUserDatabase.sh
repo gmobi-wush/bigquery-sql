33aca75acdc20c47ab7cf3c551225acab4ba2537
gmobi-wush <wush.wu@generalmobi.com>
2016-12-22 17:07:02 +0800

    Update DailyUserDatabase.sh
    remove destination before create from select

--- DailyUserDatabase.sh
+++ DailyUserDatabase.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
+(bq rm -f userdata.groupedIdAppUserData || echo "userdata.groupedIdAppUserData does not exist") && \
 cat groupedIdAppUserData.sql | bq query --nouse_legacy_sql --allow_large_results --destination_table 'userdata.groupedIdAppUserData'
